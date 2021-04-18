c6b27682cc5e91e7dc7c22f4b7dc5432e45b4b2b
Mohammedshehab7 <82756246+Mohammedshehab7@users.noreply.github.com>
2021-04-18 14:07:27 +0400

    Update MTH 243 Project Faceting.R

--- MTH 243 Project Faceting.R
+++ MTH 243 Project Faceting.R
@@ -14,7 +14,7 @@ myplot + facet_grid(cols= vars(drv), rows= vars(cyl)) #facet into both rows and 
 # You can also choose whether the scales should be constant across all panels (the default), or whether they should be allowed to vary
 mtcars
 mymtcars <- ggplot(mtcars, aes(mpg, wt, colour = factor(cyl))) + ##you can add colores
-geom_point(size= 2) ##you can adjust the size
+  geom_point(size= 2) ##you can adjust the size
 mymtcars
 mymtcars + facet_grid(vars(cyl)) #Notice: there is a color, and this is because we defined the color for cyl already.
 mymtcars + facet_grid(vars(cyl), scales = "free") ##only the axes are free
@@ -29,12 +29,13 @@ mg
 mg + facet_grid(vs + am ~ gear, margins = TRUE) ##margins vs, am, gears.
 mg + facet_grid(vs + am ~ gear, margins = "am") ##margins only am.
 ###I can do with boxplot
-ToothGrowth ggplot(ToothGrowth, aes(x=dose, y=len, group=dose)) +
-geom_boxplot(aes(fill=dose)) ##this is confusing, lets change dose to as.factor.
+ToothGrowth
+toothplot<-ggplot(ToothGrowth, aes(x=dose, y=len, group=dose)) +
+  geom_boxplot(aes(fill=dose)) ##this is confusing, lets change dose to as.factor.
+toothplot
 ToothGrowth$dose <- as.factor(ToothGrowth$dose)
 toothplot<-ggplot(ToothGrowth, aes(x=dose, y=len, group=dose)) +
-geom_boxplot(aes(fill=dose))
-toothplot
+  geom_boxplot(aes(fill=dose))
 ##facet with one variable
 # Split in vertical direction
 toothplot + facet_grid(supp ~ .)
@@ -57,27 +58,27 @@ mywrap + facet_wrap(vars(class))
 mywrap + facet_wrap(vars(class), nrow = 4)
 #You can facet by multiple variables
 ggplot(mpg, aes(displ, hwy)) + geom_point() +
-facet_wrap(vars(cyl, drv))
+  facet_wrap(vars(cyl, drv))
 ggplot(mpg, aes(displ, hwy)) + geom_point() +
-facet_wrap(vars(cyl, drv, fl))
+  facet_wrap(vars(cyl, drv, fl))
 #By default, the same scales are used for all panels. You can allow scales to vary across the panels with the `scales` argument.
 #Free scales make it easier to see patterns within each panel, but
 #harder to compare across panels.
 ggplot(mpg, aes(displ, hwy)) + geom_point() +
-facet_wrap(vars(class), scales = "free")
+  facet_wrap(vars(class), scales = "free")
 ggplot(mpg, aes(displ, hwy)) + geom_point() +
-facet_wrap(vars(class), scales = "free_x")
+  facet_wrap(vars(class), scales = "free_x")
 ggplot(mpg, aes(displ, hwy)) + geom_point() +
-facet_wrap(vars(class), scales = "free_y")
+  facet_wrap(vars(class), scales = "free_y")
 # To repeat the same data in every panel, simply construct a data frame that does not contain the faceting variable.
 ggplot(mpg, aes(displ, hwy)) + geom_point()
 ggplot(mpg, aes(displ, hwy))+
-geom_point(data = transform(mpg, class = NULL), colour = "grey85") +
-geom_point() +
-facet_wrap(vars(class))
+  geom_point(data = transform(mpg, class = NULL), colour = "grey85") +
+  geom_point() +
+  facet_wrap(vars(class))
 ###you can display the facet labels at a side of your choice
 ggplot(mpg, aes(displ, hwy)) +
-geom_point() +
-facet_wrap(vars(class), scales = "free", nrow = 2, strip.position = "bottom") +
-theme(strip.background = element_blank(), strip.placement = "outside")
+  geom_point() +
+  facet_wrap(vars(class), scales = "free", nrow = 2, strip.position = "bottom") +
+  theme(strip.background = element_blank(), strip.placement = "outside")
 ##you can do with boxplot as well, or any type of plot.
